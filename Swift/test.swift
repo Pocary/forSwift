123c8eb7ac2b7caead314c17be62920b0bcd4701
Pocary <105802972+Pocary@users.noreply.github.com>
2026-01-23 14:51:54 +0000

    [Swift] 코드 실행 환경 구현 완료 swift (파일명)으로 실행 가능

---
+++ Swift/test.swift
@@ -0,0 +1,13 @@
+var num : Int = 10
+let pi : Float = 3.14
+
+typealias Name = String
+let me : Name = "bokhal"
+var you : Name = "idiot"
+
+print(num)
+print(pi)
+print("my name is", me)
+print("you are " + you)
+print("fuck", terminator: "")
+print("off")
